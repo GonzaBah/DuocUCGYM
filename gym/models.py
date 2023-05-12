881fd9fad1d85ae1684f774fa0a5ad7aa3e9c4b8
GonzaBah <Go.bahamondes@duocuc.cl>
2023-05-11 22:44:50 -0400

    models.py listo con tablas de la base de datos

--- gym/models.py
+++ gym/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
 # Create your models here.
+
+class TipoUsuario(models.Model):
+    idTipo = models.AutoField(primary_key=True, verbose_name="ID del Tipo de Usuario")
+    nombreTipo = models.CharField(max_length=30, verbose_name="Nombre Tipo de Usuario")
+
+class Usuario(models.Model):
+    rut = models.CharField(primary_key=True, max_length=12, verbose_name="Rut del Usuario")
+    nombre = models.CharField(max_length=35, verbose_name="Nombre del Usuario")
+    apellido1 = models.CharField(max_length=30, verbose_name="Primer apellido del Usuario")
+    apellido2 = models.CharField(max_length=30, verbose_name="Segundo apellido del Usuario")
+    correo = models.EmailField(verbose_name="Correo del Usuario")
+    contrasenia = models.CharField(null=True, max_length=30, verbose_name="Contraseña")
+    tipoUsuario = models.ForeignKey(TipoUsuario, on_delete=models.SET_DEFAULT, default=0)
+    fechaNacimiento = models.DateField(verbose_name="Fecha de Nacimiento")
+    foto = models.ImageField(null=True, upload_to="\img", default="\img\avatar-redondo.png", verbose_name="Foto del Usuario")
+
+class TipoFuncionario():
+    idTipoFuncionario = models.IntegerField(primary_key=True, verbose_name="ID del Tipo Funcionario")
+
+class Funcionario():
+    idFuncionario = models.AutoField(primary_key=True, verbose_name="ID del Funcionario")
+    cargo = models.CharField(max_length=20, verbose_name="Cargo del Funcionario")
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_DEFAULT, default=0)
+    tipo = models.ForeignKey(TipoFuncionario, on_delete=models.SET_DEFAULT, default=0)
+
+class Sucursal():
+    idSucursal = models.AutoField(primary_key=True, verbose_name="ID de la Sucursal")
+    nombreSucursal = models.CharField(max_length=30, verbose_name="Nombre del Sucursal")
+    direccionSucursal = models.CharField(max_length=50, verbose_name="Dirección de la Sucursal")
+
+class Plan():
+    idPlan = models.AutoField(primary_key=True, verbose_name="ID del Plan")
+    nombrePlan = models.CharField(max_length=30, verbose_name="Nombre del Plan")
+    estadoPlan = models.BooleanField(verbose_name="Estado del Plan")
+    sucursalLibre = models.BooleanField(verbose_name="Sucursal Libre")
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.SET_DEFAULT, default=0)
+
+class Socio():
+    idSocio = models.AutoField(primary_key=True, verbose_name="ID del Socio")
+    altura = models.IntegerField(verbose_name="Altura del Socio")
+    peso = models.FloatField(verbose_name="Peso del Socio")
+    observaciones = models.CharField(max_length=100, verbose_name="Observaciones")
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_DEFAULT, default=0)
+    plan = models.ForeignKey(Plan, on_delete=models.SET_DEFAULT, default=0)
+    titularPlan = models.ForeignKey(Usuario, null=True, )
+
+class Equipamiento():
+    idEquipamiento = models.AutoField(primary_key=True, verbose_name="ID del Equipamiento")
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.SET_DEFAULT, default=0)
+
+class Reserva():
+    idReserva = models.AutoField(primary_key=True, verbose_name="ID de Reserva")
+    fechaReserva = models.DateField(verbose_name="Fecha de la Reserva")
+    horaReserva = models.TimeField(verbose_name="Hora de la Reserva")
+    socio = models.ForeignKey(Socio, on_delete=models.SET_DEFAULT, default=0)
+    equipamiento = models.ForeignKey(Equipamiento, on_delete=models.SET_DEFAULT, default=0)
+
+class TipoProfesor():
+    idTipoProfesor = models.AutoField(primary_key=True, verbose_name="ID del Tipo de Profesor")
+    nombreTipoProfesor = models.CharField(max_length=30, verbose_name="Nombre del Tipo de Profesor")
+
+class Profesor():
+    idProfesor = models.AutoField(primary_key=True, verbose_name="ID del Profesor")
+    fechaIngreso = models.DateField(verbose_name="Fecha de Ingreso")
+    fechaContrato = models.DateField(verbose_name="Fecha de Contrato")
+    tipo = models.ForeignKey(TipoProfesor, on_delete=models.SET_DEFAULT, default=0)
+    sucursal = models.ForeignKey(Sucursal, on_delete=models.SET_DEFAULT, default=0)
+
+class Deporte():
+    idDeporte = models.AutoField(primary_key=True, verbose_name="ID del Deporte")
+    nombreDeporte = models.CharField(max_length=30, verbose_name="Nombre del Deporte")
+
+class DeporteProfesor():
+    profesor = models.ForeignKey(Profesor, on_delete=models.SET_DEFAULT, default=0)
+    deporte = models.ForeignKey(Deporte, on_delete=models.SET_DEFAULT, default=0)
