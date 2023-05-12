d104523de59d557a2e74c991c141a5c97089895b
Diego Pablo Barros Araya <75908052+DiegoBarrosA@users.noreply.github.com>
2023-05-12 00:05:36 -0400

    improved tables

--- duoc_gym/settings.py
+++ duoc_gym/settings.py
@@ -74,12 +74,6 @@ WSGI_APPLICATION = 'duoc_gym.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
